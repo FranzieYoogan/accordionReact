2070cbac12af0e22cd02789e255a8813f44adba3
FranieYoogan <franzieyoogan@gmail.com>
2025-08-16 14:59:45 -0300

    done

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Accordion from './components/accordion'
+
+function App() {
+
+  return (
+    <>
+
+        <div className='App'>
+
+          <Accordion />
+          
+          </div> 
+    
+    </>
+  )
+}
+
+export default App
